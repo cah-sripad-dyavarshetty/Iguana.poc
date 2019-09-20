d0ab9de2258ff25bd25e357173a55ae92d762d72
cah-sripad-dyavarshetty <52240500+cah-sripad-dyavarshetty@users.noreply.github.com>
2019-09-20 12:42:56 +0530

    Add files via upload

---
+++ IGUANA_CHANNEL_VERSION-6_1_0/main.lua
@@ -0,0 +1,10 @@
+function main(Data)
+   -- Parse the JSON message
+   local Msg = xml.parse(Data)  
+   
+   -- (1) connect to the database
+   
+   -- (2) create insert query string
+      
+   -- (3) Insert data into database
+end
